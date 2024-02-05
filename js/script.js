2137b9558ffd32375edc5b22d530980136418c36
w3modabbir <modabbirhossen.web@gmail.com>
2024-02-05 21:10:43 +0600

    ALL Done

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ let button = document.querySelector(".input_btn");
 
 button.addEventListener("click", (e) =>{
     e.preventDefault();
+    button.innerText = "Downloding fiel...."
     fetchFile(inputUrl.value);
 })
 
 function fetchFile(url) {
-    fetch(url).then(res => res.blob()).then(file =>{
+    fetch(url).then(res => res.blob()).then(file => {
         let tempUrl = URL.createObjectURL(file);
         let a = document.createElement("a")
         a.href = tempUrl
-        a.download = "filename"
+        a.download = url.replace(/^.*[\\\/]/, '');
         document.body.appendChild(a);
         a.click();
         a.remove();
-    });
+        URL.revokeObjectURL(tempUrl);
+        button.innerText = "Downlod fiel"
+    }).catch(()=>{
+        button.innerText = "Downlod fiel"
+        alert("filed to download!")
+    })
 }
